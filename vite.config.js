b0a3f76eea81bf3507d2f895f77d71b7e01d99f3
David Gonm <148528760+DavidMacha@users.noreply.github.com>
2024-07-01 21:15:33 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
     rollupOptions: {
       external: [
         /^vercel:.*/,
-        /node_modules/,
+
         /vercel/,
-        /^node:.*/,
+
       ]
     }
   }
